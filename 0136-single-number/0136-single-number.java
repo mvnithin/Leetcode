ee91b2d7b6be1b0a1c157f57ce21b26266de0ce7
Nithin M V <112765155+mvnithin@users.noreply.github.com>
2024-10-21 07:17:09 +0530

    Time: 154 ms (8.3%), Space: 45.8 MB (69.93%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);//[1,1,2,2,4]
+        //Arrays.sort(nums);//[1,1,2,2,4]
         int ans=0;
         for(int i=0;i<nums.length;i++){
             ans^=nums[i];
